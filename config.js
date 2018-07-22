cf15cd12797fb5b8c29352e505cb52048c53eddb
danklut <danklutmeyer@hotmail.ca>
2018-07-21 18:15:18 -0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   postsPerPage: 5, // Required
   social: {
     website: 'https://aomcloudenterprise.com',
-    github: 'https://github.com/danklut,
+    github: 'https://github.com/danklut',
     twitter: 'https://twitter.com/klutmeyer',
     linkedin: 'https://ca.linkedin.com/in/danklutmeyer'
   }
