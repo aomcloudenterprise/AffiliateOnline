0f06271d3feb33d6c588d3acecfc93d8dc5cf17a
danklut <danklutmeyer@hotmail.ca>
2018-07-21 18:13:26 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
   title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
+  author: 'danklut', // Required
   tagline: 'Full-stack Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald'
+    website: 'https://aomcloudenterprise.com',
+    github: 'https://github.com/danklut,
+    twitter: 'https://twitter.com/klutmeyer',
+    linkedin: 'https://ca.linkedin.com/in/danklutmeyer'
   }
 };
