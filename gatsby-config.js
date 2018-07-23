9d0f32ae6398801bc75ff58c294719cc24b9e156
danklut <danklutmeyer@hotmail.ca>
2018-07-22 20:10:13 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,8 +37,8 @@ module.exports = {
           {
             resolve: `gatsby-source-youtube`,
             options: {
-              channelId: '<<Youtube channelID eg. UCK8sQmJBp8GCxrOtXWBpyEA >>',
-              apiKey: '<< Add your Youtube api key here>>',
+              channelId: 'UCoRYrVmWjNJ0WpbBFXMgtig',
+              apiKey: 'AIzaSyBgFTYt7t259qS8JriFBrEjPWVCpqNfkag',
               maxVideos: 50 // Defaults to 50
             },
           },
